eae013c0e52cb324fd556ac0362bfe7c1b1f6988
장동현 <54137044+ww8007@users.noreply.github.com>
2022-06-02 10:53:18 +0900

    feat: Add exaple code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Search from './Search';
 
-function App() {
+const App = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="center w85">
+			<Header />
+			<div className="ph3 pv1 background-gray">
+				<Routes>
+					<Route path="/" element={<Navigate replace to="/new/1" />} />
+					<Route path="/create" element={<CreateLink />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/top" element={<LinkList />} />
+					<Route path="/new/:page" element={<LinkList />} />
+				</Routes>
+			</div>
 		</div>
 	);
-}
+};
 
 export default App;
